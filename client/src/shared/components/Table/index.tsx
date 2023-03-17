04e6aaf5a437f9b5568415428d0d63619c241385
ragg09-sunasterisk <reneangelo.gunayon@sun-asterisk.com>
2023-03-17 17:39:12 +0800

    [LMS-333][Integration] Show Per Page

--- client/src/shared/components/Table/index.tsx
+++ client/src/shared/components/Table/index.tsx
@@ -9,8 +9,8 @@ export interface TableProps<> {
 
 const Table: React.FC<TableProps> = ({ header, children }: TableProps) => {
   return (
-    <div className="overflow-auto flex justify-center">
-      <table className="text-left text-gray-500 dark:text-gray-400">
+    <div className="overflow-auto flex justify-center border">
+      <table className="text-left text-gray-500 dark:text-gray-400 w-full">
         <thead>
           <tr>
             <th className="p-4 ">
