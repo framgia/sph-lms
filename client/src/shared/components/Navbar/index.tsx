06f590a2f139b055403aa23ee39b31339e6f8271
Miko <rosalesmikole@gmail.com>
2023-03-24 16:29:53 +0800

    [LMS-344][FE][Markup] Sign-in Page

--- client/src/shared/components/Navbar/index.tsx
+++ client/src/shared/components/Navbar/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable multiline-ternary */
 import React from 'react';
 import Link from 'next/link';
 import Avatar from '@/src/shared/components/Avatar';
@@ -18,9 +17,20 @@ export interface NavbarProps {
   isSticky?: boolean;
 }
 
-const Navbar: React.FC<NavbarProps> = ({ navItems, dropdownItems, isSticky }) => {
+const Navbar: React.FC<NavbarProps> = ({
+  navItems,
+  dropdownItems,
+  isSticky
+}) => {
+  // this is the flag for the conditional rendering
+  const signedIn =
+    typeof localStorage !== 'undefined' &&
+    localStorage.getItem('signedIn') === 'false';
+
   return (
-    <nav className={`bg-gray-100 ${(isSticky ?? false) ? 'sticky top-0' : ''} z-10`}>
+    <nav
+      className={`bg-gray-100 ${isSticky ?? false ? 'sticky top-0' : ''} z-10`}
+    >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
@@ -34,30 +44,45 @@ const Navbar: React.FC<NavbarProps> = ({ navItems, dropdownItems, isSticky }) =>
                 {navItems.map((navItem) => {
                   return (
                     <div key={navItem.url}>
-                      {navItem.dropdownItems.length > 0 ? (
+                      {navItem.dropdownItems.length > 0
+                        ? (
                         <Dropdown
                           options={navItem.dropdownItems}
                           label={navItem.text}
                         />
-                      ) : (
+                          )
+                        : (
                         <NavLink
                           href={navItem.url}
                           text={navItem.text}
                         ></NavLink>
-                      )}
+                          )}
                     </div>
                   );
                 })}
               </div>
             </div>
           </div>
-          <div className="flex items-center">
-            <Link href={'/settings'} className="text-gray-600">
-              <SettingsIcon width={20} height={20} className="mr-2" />
-            </Link>
-            <Avatar name="Jane Doe" />
-            <Dropdown options={dropdownItems} label="John Doe" />
-          </div>
+          {signedIn
+            ? (
+            <div className="flex items-center">
+              <Link href={'/settings'} className="text-gray-600">
+                <SettingsIcon width={20} height={20} className="mr-2" />
+              </Link>
+              <Avatar name="John Doe" />
+              <Dropdown options={dropdownItems} label="John Doe" />
+            </div>
+              )
+            : (
+            <div className="flex items-center">
+              <Link
+                href="/auth/sign-in"
+                className="bg-slate-300 py-1.5 px-3 text-gray rounded hover:bg-slate-500 hover:text-white"
+              >
+                Sign in
+              </Link>
+            </div>
+              )}
         </div>
       </div>
     </nav>
