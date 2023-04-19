1ba59de70e4d6fa53ba081f527cba8b3d7c6acfe
root <root@DESKTOP-U17V5RI>
2023-04-19 14:34:09 +0800

    Finish tasks

--- client/src/pages/trainer/course/[id]/enrollUser.tsx
+++ client/src/pages/trainer/course/[id]/enrollUser.tsx
@@ -1,45 +1,58 @@
-import React, { Fragment, useEffect, useState } from 'react';
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+import React, { Fragment } from 'react';
+
 import Navbar from '@/src/shared/components/Navbar';
 import Container from '@/src/shared/layouts/Container';
 import Table from '@/src/shared/components/Table';
-import { dropdownItems, navItems } from '@/src/pages/demo/layouts/navbar';
-import API from '@/src/apis';
-import { type User } from '@/src/shared/utils';
+import Select from '@/src/shared/components/Select';
+import { dropdownItems, navItems } from '@/src/shared/utils/navBarList';
+import Breadcrumbs from '@/src/shared/components/Breadcrumbs';
+import NotFoundPage from '@/src/pages/404';
+import useEnrollUser from '@/src/shared/hooks/useEnrollUser';
 
 const EnrollUser: React.FC = () => {
-  const [users, setUsers] = useState<User[]>([]);
+  const {
+    paths,
+    courseTitle,
+    showPerPage,
+    handleShowPerPage,
+    showPerPageOption,
+    pageNotFound
+  } = useEnrollUser();
 
-  useEffect(() => {
-    let ignore = false;
-    async function fetchData (): Promise<void> {
-      const result = await API.get('user/1');
-      if (!ignore) {
-        setUsers(result.data.user);
-        console.log(result.data.user);
-      }
-    }
-
-    void fetchData();
-
-    return () => {
-      ignore = true;
-    };
-  }, []);
+  if (pageNotFound) {
+    return (
+      <NotFoundPage />
+    );
+  }
 
   return (
     <Fragment>
       <Navbar navItems={navItems} dropdownItems={dropdownItems} />
       <Container>
+        <div className='mb-10 mt-2'>
+          <Breadcrumbs paths={paths} />
+        </div>
+        <div>
+          <h1 className='text-2xl text-blue-500 pb-10 pl-2'>Enroll Users into {courseTitle}</h1>
+        </div>
         <Table
-          header={[{ text: 'First Name' }, { text: 'Last Name' }, { text: 'Email' }, { text: 'Role' }]}
+          header={[{ text: 'First Name' }, { text: 'Last Name' }, { text: 'Email' }, { text: 'Creation Date' }, { text: 'Role' }]}
         >
-         {Array.isArray(users) && users.length > 0
+         {Array.isArray(showPerPage) && showPerPage.length > 0
            ? (
-               users.map((col: any) => (
+               showPerPage.map((col: any) => (
                   <tr
                     className="border-b whitespace-nowrap text-sm text-black1 font-sans h-5"
                     key={col.id}
                   >
+                    <td className="px-6 py-4">
+                  <input
+                  type="checkbox"
+                  className="h-5 w-5 hover:bg-sky-700"
+                  ></input>
+              </td>
                     <td className="px-6 py-4 text-lightBlue underline">
                       {col.first_name}
                     </td>
@@ -47,10 +60,10 @@ const EnrollUser: React.FC = () => {
                       {col.last_name}
                     </td>
                     <td className="px-6 py-4 text-lightBlue underline">
-                      {col.created_at}
+                      {col.email}
                     </td>
                     <td className="px-6 py-4 text-lightBlue underline">
-                      {col.email}
+                    {col.created_at}
                     </td>
                     <td className="px-6 py-4">{col.role.title}</td>
                   </tr>
@@ -66,6 +79,14 @@ const EnrollUser: React.FC = () => {
                 </tr>
              )}
         </Table>
+        <div className="flex justify-end mt-3">
+          <Select
+            width="200px"
+            eventHandler={handleShowPerPage}
+            label="Show Per Page"
+            options={showPerPageOption}
+          />
+        </div>
       </Container>
     </Fragment>
   );
