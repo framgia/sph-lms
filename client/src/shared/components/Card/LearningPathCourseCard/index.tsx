bdeca3d707909345fb244615325df7bb585007db
jules-lucero <julesrussel.lucero@sun-asterisk.com>
2023-06-05 11:52:38 +0800

    Set 128px minimum height to LearningPathCourseCard

--- client/src/shared/components/Card/LearningPathCourseCard/index.tsx
+++ client/src/shared/components/Card/LearningPathCourseCard/index.tsx
@@ -9,8 +9,8 @@ interface Props {
 
 const LearningPathCourseCard = ({ imgPath, courseTitle, lessonsCount }: Props): JSX.Element => {
   return (
-    <div className="flex w-fit min-w-[420px] overflow-hidden bg-white border rounded-[5px] border-neutral-100">
-      <Image src={imgPath} width={150} height={150} alt={courseTitle} />
+    <div className="flex w-fit min-w-[420px] min-h-[128px] overflow-hidden bg-white border rounded-[5px] border-neutral-100">
+      <Image src={imgPath} width={150} height={150} alt={courseTitle} className="object-cover" />
       <div className="w-full p-4 flex flex-col gap-2">
         <h3>{courseTitle}</h3>
         <div className="flex flex-col text-xs">
