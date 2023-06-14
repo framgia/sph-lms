7feedc6b10e349aeed07e1d89fcb3e344f41a294
Nicole Amber Mariscal <116238730+nicoleamber@users.noreply.github.com>
2023-06-14 13:02:36 +0800

    [LMS-650] [Markup] Trainer Dashboard (#130)

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,5 +1,5 @@
+import { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
-import { Html, Head, Main, NextScript } from 'next/document';
 
 const Document: React.FunctionComponent = () => {
   return (
