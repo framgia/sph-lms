b3ab7cff1fcb28b61019c64763ac9ac92b4e22ba
ragg09-sunasterisk <115448429+ragg09-sunasterisk@users.noreply.github.com>
2023-06-16 11:10:57 +0800

    [LMS-652][BE] Assign Trainers and trainees using django/python command (#137)
    * [LMS-652][BE] Assign Trainers and trainees using django/python command
    
    * adjustments

---
+++ api/app_sph_lms/management/commands/usertrainee.py
@@ -0,0 +1,130 @@
+from app_sph_lms.models import Course, LearningPath, User
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Assign trainees to Courses or Learning Paths.'
+
+    def add_arguments(self, parser):
+
+        parser.add_argument(
+            'email',
+            type=str,
+            help='Email of trainer'
+        )
+
+        parser.add_argument(
+            '-c',
+            '--course',
+            type=str,
+            required=False,
+            help='Assign trainee to a course. Please wrap the \
+                course with double quotes. e.g "Example Course 1"'
+        )
+
+        parser.add_argument(
+            '-lp',
+            '--learning-path',
+            type=str,
+            required=False,
+            help='Assign trainee to a learning path. Please wrap the \
+                learning path with double quotes. e.g "Example Leaning Path"'
+        )
+
+    def handle(self, *args, **kwargs):
+        email = kwargs['email']
+        course = kwargs['course']
+        learning_path = kwargs['learning_path']
+        is_valid = True
+
+        try:
+            user = User.objects.get(email=email)
+            if user.is_trainer:
+                return self.stdout.write(
+                    self.style.ERROR(
+                        f'User with email {email} is a trainer. \
+                            Please use trainee instead.'
+                    )
+                )
+        except User.DoesNotExist:
+            return self.stdout.write(
+                self.style.ERROR(
+                    f'User with email {email} does not exist.'
+                )
+            )
+
+        if not course and not learning_path:
+            self.stdout.write(
+                    "Can insert both course [ -c | --course ] "
+                    "and learning path [ -lp | --learning-path ] "
+                    "at the same time."
+                )
+            self.stdout.write("Use help [ -h ] for more information.")
+            return self.stdout.write(
+                self.style.ERROR(
+                    'Please specify either a course or a learning path.'
+                )
+            )
+
+        if course:
+            try:
+                check_course = Course.objects.get(name=course)
+
+                if check_course.trainee.filter(email=email).exists():
+                    self.stdout.write(
+                        self.style.WARNING(
+                            f'{email} is already enrolled in "{course}".'
+                        )
+                    )
+                    is_valid = False
+                else:
+                    self.stdout.write(f'Enrolling {email} to {course} . . .')
+                    check_course.trainee.add(user)
+            except Course.DoesNotExist:
+                self.stdout.write(
+                    self.style.WARNING(
+                        f'{course} does not exist.'
+                    )
+                )
+                is_valid = False
+
+        if learning_path:
+            try:
+                check_learning_path = LearningPath.objects.get(
+                        name=learning_path
+                    )
+
+                if check_learning_path.trainee.filter(email=email).exists():
+                    self.stdout.write(
+                        self.style.WARNING(
+                            f'{email} is already '
+                            f'enrolled in "{learning_path}".'
+                        )
+                    )
+                    is_valid = False
+                else:
+                    self.stdout.write(
+                            f'Enrolling {email} to {learning_path} . . .'
+                        )
+                    check_learning_path.trainee.add(user)
+            except LearningPath.DoesNotExist:
+                self.stdout.write(
+                    self.style.WARNING(
+                        f'{learning_path} does not exist.'
+                    )
+                )
+                is_valid = False
+
+        if is_valid:
+            return self.stdout.write(
+                    self.style.SUCCESS(
+                            f'{email} successfully enrolled to '
+                            'course and/or learning path'
+                        )
+                )
+        else:
+            return self.stdout.write(
+                    self.style.ERROR(
+                            'Process failed, no entry inserted.'
+                        )
+                )
---
+++ api/app_sph_lms/management/commands/usertrainer.py
@@ -0,0 +1,95 @@
+from app_sph_lms.models import User
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = 'Assign trainees to a trainer'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'email',
+            type=str,
+            help='Email of trainer'
+        )
+
+        parser.add_argument(
+            '-at',
+            '--assign-trainee',
+            type=str,
+            nargs='+',
+            required=False,
+            help='Input trainee '
+        )
+
+    def handle(self, *args, **kwargs):
+        email = kwargs['email']
+        assign_trainee = kwargs['assign_trainee']
+
+        try:
+            user = User.objects.get(email=email)
+            if not user.is_trainer:
+                return self.stdout.write(
+                    self.style.ERROR(
+                        f'User with email {email} is not a trainer.'
+                    )
+                )
+        except User.DoesNotExist:
+            return self.stdout.write(
+                self.style.ERROR(
+                    f'User with email {email} does not exist.'
+                )
+            )
+
+        if not assign_trainee:
+            raise CommandError(
+                '--assign-trainee (-at) option is required. '
+                'Run help for more information.'
+            )
+
+        if assign_trainee:
+            is_valid = True
+
+            for trainee_email in assign_trainee:
+                if ',' in trainee_email:
+                    raise CommandError(
+                            'Comma is not allowed in'
+                            f' trainee emails: {trainee_email}.'
+                        )
+
+                try:
+                    check_user = User.objects.get(email=trainee_email)
+
+                    if check_user.trainer_id is not None \
+                            or check_user.is_trainer:
+                        self.stdout.write(
+                            self.style.WARNING(
+                                f'User with email {trainee_email}'
+                                ' is trainee already.'
+                            )
+                        )
+                        is_valid = False
+                except User.DoesNotExist:
+                    self.stdout.write(
+                        self.style.WARNING(
+                            f'User with email {trainee_email} does not exist.'
+                        )
+                    )
+                    is_valid = False
+
+            if is_valid:
+                for trainee_email in assign_trainee:
+                    self.stdout.write(f'Inserting {trainee_email} . . .')
+                    trainee = User.objects.get(email=trainee_email)
+                    trainee.trainer_id = user
+                    trainee.save()
+                return self.stdout.write(
+                        self.style.SUCCESS(
+                                f'The emails are now trainee of {email}.'
+                            )
+                    )
+            else:
+                return self.stdout.write(
+                        self.style.ERROR(
+                                'Process failed, no entry inserted.'
+                            )
+                    )
